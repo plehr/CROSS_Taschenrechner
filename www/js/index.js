b03dcd40058155aefe537475f2f71597954e0611
PLehr.de <spam@plehr.de>
2021-03-26 14:41:50 +0100

    added JS logic with event handler

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,8 @@
-document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener('deviceready', init, false);
 var btn_sum;
 var field_input;
+var nextReset = false;
 
-function onDeviceReady() {
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    init();
-}
 function init() {
     field_input = document.getElementById("in")
     btn_sum = document.getElementById("btn_sum")
@@ -18,14 +15,22 @@ function init() {
     addHandler(document.getElementById("btn_div"), "/")
 
     btn_sum.addEventListener("click", function () {
+        nextReset = true;
         document.getElementById("in").value = sum()
     })
 }
+
 function sum() {
-    //TODO add sum function with eval
+    console.log("Ich berechne: " + field_input.value + " und komme auf das Ergebnis: " + eval(field_input.value))
+    return eval(field_input.value)
 }
+
 function addHandler(btn, num) {
     btn.addEventListener("click", function () {
+        if (nextReset) {
+            field_input.value = ""
+            nextReset = false;
+        }
         field_input.value += "" + num
     })
 }
